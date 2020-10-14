8f83b2343518f386d595acad0d1e2fcfc40718e3
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-10-14 15:44:48 -0500

    Adds `UnitTestCase` template

---
+++ README.md
@@ -0,0 +1,3 @@
+## How to install templates?
+
+Just copy the desired template to `~/Library/Developer/Xcode/Templates/`
---
+++ UnitTestCase.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,14 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+import XCTest
+
+final class ___FILEBASENAMEASIDENTIFIER___Tests: XCTestCase {
+    func test_anAction_doesSomething() {
+        XCTAssertTrue(false, "Expected true but got false instead.")
+    }
+}
