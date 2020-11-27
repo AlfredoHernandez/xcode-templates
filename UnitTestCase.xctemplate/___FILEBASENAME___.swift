5743a4642a91b73079b0d9ba7419b39ae17e668f
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-11-26 21:27:07 -0600

    Delete `Test` word from test class

--- UnitTestCase.xctemplate/___FILEBASENAME___.swift
+++ UnitTestCase.xctemplate/___FILEBASENAME___.swift
@@ -5,10 +5,11 @@
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  ___COPYRIGHT___
 //
+
 import XCTest
 
-final class ___FILEBASENAMEASIDENTIFIER___Tests: XCTestCase {
-    func test_anAction_doesSomething() {
-        XCTAssertTrue(false, "Expected true but got false instead.")
+final class ___FILEBASENAMEASIDENTIFIER___: XCTestCase {
+    func test() {
+        XCTFail("Initial test")
     }
 }
