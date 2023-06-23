3374b91e1fe590dd4079a2d489286619ce683253
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2023-06-23 16:12:47 -0600

    feat: Update test template

--- Templates/Unit Test Case.xctemplate/___FILEBASENAME___.swift
+++ Templates/Unit Test Case.xctemplate/___FILEBASENAME___.swift
@@ -5,8 +5,9 @@
 @testable import ___PROJECTNAME:identifier___
 import XCTest
 
+@MainActor
 final class ___FILEBASENAMEASIDENTIFIER___: XCTestCase {
-    func test_zero() {
+    func test_zero() throws {
         XCTFail("Tests not yet implemented in ___FILEBASENAMEASIDENTIFIER___")
     }
 }
