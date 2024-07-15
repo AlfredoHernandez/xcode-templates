3b29a6f3151e76b3a7ea8c4252203348d810ecdb
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2024-07-15 11:34:01 -0600

    Add Swift Testing template

---
+++ Templates/Swift Testing Test.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,8 @@
+@testable import ___PROJECTNAME:identifier___
+import Testing
+
+struct ___FILEBASENAMEASIDENTIFIER___ {
+    @Test("test zero") func zero() async throws {
+        Issue.record("Tests not yet implemented in ___FILEBASENAMEASIDENTIFIER___")
+    }
+}
